d82cd6a63274f4e438d0716d7c3b2cb5f657e664
fadlyoktario25 <fadlyoktario25@gmail.com>
2026-08-16 17:54:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -174,7 +174,7 @@ if uploaded_files:
                     """
 
                     response = client.models.generate_content(
-    model="gemini-flash-latest",
+    model="gemini-3.7-flash",
     contents=[*gemini_files, prompt],
 )
 
